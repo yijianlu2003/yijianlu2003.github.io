5fd00038552b75644016d9937bb55ef9a8f23220
Yijian Lu <yijianlu@connect.hku.hk>
2026-09-09 16:51:19 +0800

    Ship classic academic design at root; fix sitemap namespace and awards grid

--- src/build.mjs
+++ src/build.mjs
@@ -187,7 +187,7 @@ function sitemap(pages) {
     )
     .join('\n');
   return `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.w3.org/1999/xhtml/sitemap" xmlns:xhtml="http://www.w3.org/1999/xhtml">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
 ${urls}
 </urlset>`;
 }
